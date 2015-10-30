2695ce7bd07559279f4cb65e9498c9c9246f9ed6
mikkuri21 <nalsengom@daum.net>
2015-10-30 17:43:26 +0900

    HotFix 버그수정 했음

--- src/main/java/com/hybrid/Log4JTest.java
+++ src/main/java/com/hybrid/Log4JTest.java
@@ -9,8 +9,8 @@ public class Log4JTest {
 
 	public static void main(String[] args) {
 		log.info("Hello Log4j2 ");
-		for(int i=0; i<10; i++){
-			log.info("i = " + i);
+		for(int i=0; i<100; i++){
+			log.info("index = " + i);
 		}
 	}
 
